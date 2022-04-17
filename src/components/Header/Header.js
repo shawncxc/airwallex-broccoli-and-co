720f64372b17b0e1a71b2f407dbbed87777d1a96
xuchangchen <imxuchang@gmail.com>
2022-04-17 12:52:14 +0800

    fix lint

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 export default function Header({ title }) {
   return (
     <div className={ styles.container }>
-      <img src={process.env.PUBLIC_URL + '/logo512.jpeg'} className={ styles.logo } />
+      <img src={process.env.PUBLIC_URL + '/logo512.jpeg'} className={ styles.logo } alt="logo" />
       <div>{ title }</div>
     </div>
   );
