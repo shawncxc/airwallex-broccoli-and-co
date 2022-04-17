bfa9832c1001afe26497034ba754776d88b84592
xuchangchen <imxuchang@gmail.com>
2022-04-17 12:51:22 +0800

    detailing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import styles from "./styles.module.css";
 export default function Header({ title }) {
   return (
     <div className={ styles.container }>
+      <img src={process.env.PUBLIC_URL + '/logo512.jpeg'} className={ styles.logo } />
       <div>{ title }</div>
     </div>
   );
